13f545598f42647f650472f63034eb1cd8643819
student <student@info-09.cshl.edu>
2022-10-11 12:46:19 -0400

    script to print name and sys.argv

--- my_first_python_script.py
+++ my_first_python_script.py
@@ -17,6 +17,7 @@ print('My favorite activity:', my_activity)
 my_animal = 'Deer'
 print ('My favorite animal:', my_animal)
 
+# this argument allows script to be more general. You can provide the argument from the terminal and do not need to precise it in the script. when you run this script it will require to provide the varibles -- python3 (is not a sys arg), file.py (argv 0), [1], [2] etc..
 name = sys.argv[1]
 color = sys.argv[2]
 activity = sys.argv[3]
