95117c5a647f0d2a446109f5485992b5431c21cf
student <student@info-09.cshl.edu>
2022-10-17 21:26:13 -0400

    added script with good working cut site finder

--- my_31st_python_script 2.py
+++ my_31st_python_script 2.py
@@ -13,16 +13,19 @@ with open("Python_07_ApoI.fasta","r") as ApoI:
 			sequence = line
 			seq[gene_id]+=sequence
 print(seq)
-for gene_id in seq:
-	found = re.findall(r"(.{10})([AG]AATT[CT])(.{10})", seq[gene_id])# idicate that the search needs to be done in values, not in the entire document # if you use #findall you find all the potential matches and they will be printed
-	if found:
-		print(found)
-		print("Total number of sites :",(len(found)))
-	#site = found.group(2)
-	#print("cut site: ", found.group(2))
-	#upstream = found.group(1)
-	#print("upstream of the site: ", upstream)
-	#downstream = found.group(3)
-	#print("downsream of the site: ", downstream)
+for site in re.finditer(r"([AG])(AATT)([CT])", seq[gene_id]):
+	#if found in seq:
+	#print(found)
+	#print("Total number of sites :", (len(found)))
+	#print("site; ", site.group(2))
+	#print("upstream of the site: ", site.group(1))
+	#print("downsream of the site: ", site.group(3))
+	whole = site.group(0)
+	print("entire site: ", whole)
+	repl = re.sub(r"([AG])(AATT)([CT])", r"\1^\2\3", seq[gene_id]) # this substitutes patterm to another thing
+	print (repl)
+	fragments = repl.split('^')
+	print (fragments)
+	print(sorted(fragments, key=len, reverse=False))
 	#print(f" This is the cut region {upstream}{site}{downstream}")
 
